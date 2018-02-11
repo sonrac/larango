40d9357ca93c74a7a2eabe02ed6d987984ca752f
n.marshak <n.marshak@conci.info>
2018-02-12 00:34:31 +0200

    add .htaccess to git ignore (maked from apache docker)

---
+++ src/Eloquent/Builder.php
@@ -0,0 +1,14 @@
+<?php
+namespace sonrac\Arango\Eloquent;
+
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 21.01.2018
+ * Time: 14:42
+ */
+
+class Builder extends \Illuminate\Database\Eloquent\Builder
+{
+
+}
